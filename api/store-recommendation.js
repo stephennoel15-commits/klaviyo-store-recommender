5e69c3b049c60ed5bc7a6d411c408d0866f984bc
stephennoel15-commits <stephennoel15@gmail.com>
2026-03-16 11:47:10 -0500

    store-recommendation.js

---
+++ api/store-recommendation.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+
+ if (req.method !== "POST") {
+   return res.status(405).json({ error: "Method not allowed" })
+ }
+
+ return res.status(200).json({
+   message: "API working"
+ })
+
+}
